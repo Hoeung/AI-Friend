0c94d74a2c5f774856d7c4174dcbbdc49345433b
administrator <B20207.tdc.hanmacgroup@gmail.com>
2024-07-12 15:04:23 +0900

    1. Jamesnet.WPF Nuget Package 설치 2. 새로운 방식의 기본 창 띄우는 방식으로 변경. 3. WPF 프로젝트 기본 생성시 생성되는 파일들 삭제.

---
+++ AI-Friend/App.cs
@@ -0,0 +1,13 @@
+﻿using Jamesnet.Wpf.Controls;
+using System.Windows;
+
+namespace AI_Friend;
+
+internal class App : JamesApplication
+{
+    protected override Window CreateShell()
+    {
+        // Temporary Window instance
+        return new Window();
+    }
+}
---
+++ AI-Friend/Starter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AI_Friend;
+
+internal class Starter
+{
+    [STAThread]
+    private static void Main(string[] args)
+    {
+        if (args is null)
+        {
+            throw new ArgumentNullException(nameof(args));
+        }
+
+        _ = new App().Run();
+    }
+}
